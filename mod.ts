c0c3b8bf88bf4c046ea4de7e4d39b2e68056695c
Jake O'Shannessy <joshannessy@gmail.com>
2024-06-22 14:11:26 +0100

    Switch to typst output (v0.1.6)

--- mod.ts
+++ mod.ts
@@ -1,20 +1,16 @@
 import {
-  type FdsFile,
-  type InputSummary,
-  type Resolution,
-  summarise_input,
-  type VerificationSummary,
-  verifyInput,
-} from "jsr:@smoke-cloud/fds-inspect-core@0.1.3";
-import Handlebars from "npm:handlebars@4.7.8";
-import {
-  summaryTableHbs,
-  verificationListHbs,
-  verificationPageHbs,
-  verificationTableHbs,
-} from "./hbs.ts";
+  clearSuccessSummary,
+  type VerificationOutcome,
+  type VerificationResult,
+} from "jsr:@smoke-cloud/fds-inspect-core@0.1.6";
+import type * as fdsInpectCore from "jsr:@smoke-cloud/fds-inspect-core@0.1.6";
+import type { InputSummary } from "../fds-inspect-core/summary.ts";
+import * as path from "jsr:@std/path@0.225.2";
 
-export async function getJson(path: string, cwd?: string): Promise<FdsFile> {
+export async function getJson(
+  path: string,
+  cwd?: string,
+): Promise<fdsInpectCore.fds.FdsFile> {
   let s;
   try {
     const cmd = "fds-verify.cmd";
@@ -37,89 +33,160 @@ export async function getJson(path: string, cwd?: string): Promise<FdsFile> {
   }
 }
 
-export function renderVerification(
-  inputSummary: InputSummary,
-  verificationSummary: VerificationSummary,
-): string {
-  Handlebars.registerHelper("renderCategory", renderCategory);
-  Handlebars.registerHelper("categoryClass", categoryClass);
-  Handlebars.registerHelper("renderResolution", renderResolution);
-  Handlebars.registerHelper("renderHoC", renderHoC);
-  Handlebars.registerHelper("renderHrr", renderHrr);
-  Handlebars.registerHelper("renderSootProduction", renderSootProduction);
-  Handlebars.registerHelper("renderInteger", renderInteger);
-  Handlebars.registerHelper("renderFloat", renderFloat);
-
-  Handlebars.registerPartial("verificationTable", verificationTableHbs);
-  Handlebars.registerPartial("verificationList", verificationListHbs);
-  Handlebars.registerPartial("summaryTable", summaryTableHbs);
-
-  const template = Handlebars.compile(verificationPageHbs);
-
-  function renderCategory(category: string): string | undefined {
-    switch (category) {
-      case "failure":
-        return "[Failure]";
-      case "warning":
-        return "[Warning]";
-      case "success":
-        return "[Success]";
-      default:
-        break;
-    }
-  }
-
-  function categoryClass(category: string): string | undefined {
-    switch (category) {
-      case "failure":
-        return "shown-test";
-      case "warning":
-        return "shown-test";
-      case "success":
-        return "hidden-test";
-      default:
-        break;
-    }
-  }
-
-  function renderResolution(resolution: Resolution): string | undefined {
-    return `${resolution.dx.toFixed(3)}×${resolution.dy.toFixed(3)}×${
-      resolution.dz.toFixed(3)
-    } m`;
-  }
-
-  /** Render HoC as MJ/kg */
-  function renderHoC(hoc: number): string {
-    return (hoc / 1000 / 1000).toLocaleString("en-US", {
-      minimumFractionDigits: 2,
-    });
-  }
-
-  /** Render HRR as kW */
-  function renderHrr(hrr: number): string {
-    return (hrr / 1000).toLocaleString("en-US", { minimumFractionDigits: 2 });
-  }
-
-  function renderSootProduction(sootProduction: number): string {
-    return (sootProduction * 1000).toLocaleString("en-US", {
-      minimumFractionDigits: 2,
-    });
-  }
-
-  function renderInteger(i: number): string {
-    return i.toLocaleString("en-US");
-  }
-
-  function renderFloat(i: number): string {
-    return i.toLocaleString("en-US", { minimumFractionDigits: 2 });
-  }
-
-  return template({ inputSummary, verificationSummary });
+export async function getJsonTemp(inputPath: string) {
+  const tempDir = await Deno.makeTempDir();
+  const fn = path.basename(inputPath);
+  await Deno.copyFile(inputPath, path.join(tempDir, fn));
+  return await getJson(fn, tempDir);
 }
 
-export function verifyInputRender(fdsFile: FdsFile): string {
-  const verificationSummary = verifyInput(fdsFile);
-  const inputSummary = summarise_input(fdsFile);
-  const rendered = renderVerification(inputSummary, verificationSummary);
-  return rendered;
+export async function renderTypstPdf(path: string, typstInput: string) {
+  const cmd = new Deno.Command("typst", {
+    args: ["compile", "-", path],
+    stdin: "piped",
+    stdout: "piped",
+    stderr: "piped",
+  });
+  const child = cmd.spawn();
+  {
+    const writer = child.stdin.getWriter();
+    await writer.write((new TextEncoder()).encode(typstInput));
+    writer.close();
+  }
+  const output = await child.output();
+  if (output.success) {
+    const stdout = (new TextDecoder()).decode(output.stdout);
+    console.log(stdout);
+  } else {
+    const stderr = (new TextDecoder()).decode(output.stderr);
+    console.log(stderr);
+  }
+}
+
+export function renderVerificationTypst(
+  inputSummary: InputSummary,
+  verificationSummary: VerificationOutcome[],
+): string {
+  let s = "";
+  // Title
+  s +=
+    `#set document(title: [${inputSummary.chid}], author: "Smoke Cloud")\n\n`;
+  s += `= ${inputSummary.chid}\n\n`;
+  s += `== Input Summary\n\n`;
+  s += `#table(\n`;
+  s += `
+  columns: (auto,  auto),
+  inset: 10pt,
+  align: horizon,
+  table.header(
+    [*Parameter*], [*Value*],
+  ),
+`;
+  s += `  "CHID",\n`;
+  s += `  "${inputSummary.chid}",\n`;
+
+  s += `  "Simulation Time",\n`;
+  s += `  [${inputSummary.simulation_length.toLocaleString("en-US")} s],\n`;
+
+  s += `  "# Burners",\n`;
+  s += `  [${inputSummary.n_burners}],\n`;
+
+  s += `  "Total Max. HRR",\n`;
+  s += `  [${
+    (inputSummary.total_max_hrr / 1000).toLocaleString("en-US")
+  } kW ],\n`;
+
+  s += `  [Heat of Combustion],\n`;
+  s += `  [${
+    (inputSummary.heat_of_combustion / 1000 / 1000).toLocaleString("en-US")
+  } MJ/kg ],\n`;
+
+  s += `  [Total Max. Soot Production],\n`;
+  s += `  [${
+    (inputSummary.total_soot_production * 1000).toLocaleString("en-US")
+  } g/s ],\n`;
+
+  s += `  [Non-Dimensionalised Ratios],\n`;
+  s += `  [ ],\n`;
+
+  s += `  [\\# Sprinklers],\n`;
+  s += `  [${inputSummary.n_sprinklers}],\n`;
+
+  s += `  [Sprinkler Activation Temperatures],\n`;
+  s += `  [${
+    inputSummary.sprinkler_activation_temperatures.map((c) =>
+      c.toFixed() + "°C"
+    ).join("\n\n")
+  }],\n`;
+
+  s += `  [\\# Detectors],\n`;
+  s += `  [${inputSummary.n_smoke_detectors}],\n`;
+
+  s += `  [Smoke Detector Obscurations],\n`;
+  s += `  [${
+    inputSummary.smoke_detector_obscurations.map((c) => c.toFixed() + " %Obs/m")
+      .join("\n\n")
+  } ],\n`;
+
+  s += `  [\\# Extracts],\n`;
+  s += `  [${inputSummary.n_extract_vents}],\n`;
+
+  s += `  [Total Extract Rate],\n`;
+  s += `  [${inputSummary.total_extract_rate.toFixed(2)} m³/s],\n`;
+
+  s += `  [\\# Supplies],\n`;
+  s += `  [${inputSummary.n_supply_vents}],\n`;
+
+  s += `  [Total Supply Rate],\n`;
+  s += `  [${inputSummary.total_supply_rate.toFixed(2)} m³/s],\n`;
+
+  s += `  [\\# Meshes],\n`;
+  s += `  [${inputSummary.n_meshes}],\n`;
+
+  s += `  [\\# Cells],\n`;
+  s += `  [${inputSummary.n_cells.toLocaleString("en-US")} ],\n`;
+
+  s += `  [Mesh Resolutions],\n`;
+  s += `  [
+    ${
+    inputSummary.mesh_resolutions.map((c) =>
+      `${c.dx.toFixed(3)}×${c.dy.toFixed(3)}×${c.dz.toFixed(3)} m`
+    ).join("\n\n")
+  }
+  ],\n`;
+  s += `)\n\n`;
+  s += `== Failed Verification Tests\n\n`;
+  const res = clearSuccessSummary(verificationSummary);
+  if (res.length > 0) {
+    for (const testResult of res) {
+      s += renderTest(testResult);
+    }
+  } else {
+    s += "No failed tests\n\n";
+  }
+  s += `== All Verification Tests\n\n`;
+  if (verificationSummary) {
+    for (const r of verificationSummary) {
+      s += renderTest(r);
+    }
+  }
+  return s;
+}
+
+function renderTest(v: VerificationResult & { id: string }) {
+  if (!v) return "";
+  let s = "";
+  let color;
+  if (v.type === "success") {
+    color = 'rgb("#ceffc4")';
+  } else if (v.type === "warning") {
+    color = 'rgb("#fff3c4")';
+  } else {
+    color = 'rgb("#ffcbc4")';
+  }
+  s +=
+    `#highlight(fill: ${color},bottom-edge: "descender", radius: 0.1em, extent: 0.2em)[#text(smallcaps[${v.type}])]  `;
+  s += `\`${v.id}\`\n\n`;
+  s += `#par(first-line-indent: 2em, hanging-indent:2em)[${v.message}]\n\n`;
+  return s;
 }
