efcff9de647996152b22b7786cd40993cd6d16e1
Jake O'Shannessy <joshannessy@gmail.com>
2024-08-08 20:45:45 +0100

    update deps (v0.1.18)

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import * as fdsInspectCore from "jsr:@smoke-cloud/fds-inspect-core@0.1.15";
+import * as fdsInspectCore from "jsr:@smoke-cloud/fds-inspect-core@0.1.16";
 import * as path from "jsr:@std/path@0.225.2";
 
 export async function getJson(
